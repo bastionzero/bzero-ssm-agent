de07432c14fff83e5d920d874fc62d08339ba0ac
Yuval Marcus <ymarcus93@gmail.com>
2021-07-09 11:13:41 -0400

    Add chunker algorithm to be used in FUD-download

---
+++ agent/keysplitting/chunker.go
@@ -0,0 +1,80 @@
+package keysplitting
+
+import (
+	"bufio"
+	"context"
+	"fmt"
+	"io"
+	"os"
+)
+
+type ChunkResult struct {
+	Result Chunk
+	Error  error
+}
+
+type Chunk struct {
+	Data   []byte
+	Offset int
+}
+
+// ReadFileChunkwise reads a file located at filePath and splits it into chunks
+// of size chunkSizeBytes. It returns an error if the OS fails to open the file.
+//
+// ReadFileChunkwise blocks on sending to chunksRead if the channel is full or
+// if it is unbuffered and no goroutine is ready to receive the next chunk. It
+// will not block if the context is cancelled.
+//
+// A ChunkResult with Error == io.EOF will be sent on chunksRead when the file
+// has been read in its entirety.
+func ReadFileChunkwise(ctx context.Context, filePath string, chunkSizeBytes int, chunksRead chan<- ChunkResult) error {
+	// Open file
+	file, err := os.Open(filePath)
+	if err != nil {
+		return fmt.Errorf("failed to open file: %v", err)
+	}
+
+	// Start a goroutine to split file into chunks
+	go func() {
+		defer file.Close()
+		reader := bufio.NewReader(file)
+		splitReaderIntoChunks(ctx, reader, chunkSizeBytes, chunksRead)
+	}()
+
+	return nil
+}
+
+func splitReaderIntoChunks(ctx context.Context, reader io.Reader, chunkSizeBytes int, chunksRead chan<- ChunkResult) {
+	var offset int = 0
+	for {
+		buffer := make([]byte, chunkSizeBytes)
+		bytesRead, err := reader.Read(buffer)
+		if err != nil {
+			if err != io.EOF {
+				select {
+				case chunksRead <- ChunkResult{Result: Chunk{}, Error: err}:
+					return
+				case <-ctx.Done():
+					return
+				}
+			} else {
+				// Hit EOF. Send special result
+				select {
+				case chunksRead <- ChunkResult{Result: Chunk{}, Error: io.EOF}:
+					return
+				case <-ctx.Done():
+					return
+				}
+			}
+		} else {
+			select {
+			case chunksRead <- ChunkResult{Result: Chunk{Data: buffer[:bytesRead], Offset: offset}, Error: nil}:
+				break
+			case <-ctx.Done():
+				return
+			}
+
+			offset += bytesRead
+		}
+	}
+}
---
+++ agent/keysplitting/chunker_test.go
@@ -0,0 +1,91 @@
+package keysplitting
+
+import (
+	"bytes"
+	"context"
+	"errors"
+	"io"
+	"io/ioutil"
+	"log"
+	"math/rand"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func randomReader(t *testing.T, numBytes int) (io.Reader, []byte) {
+	buf := make([]byte, numBytes)
+	_, err := rand.Read(buf)
+	if err != nil {
+		t.Fatalf("failed to generate random data: %v", err)
+	}
+
+	return bytes.NewReader(buf), buf
+}
+
+// ErrReader returns an io.Reader that returns 0, err from all Read calls. Note:
+// Use built-in err reader from iotest package when we move to 1.16
+func ErrReader(err error) io.Reader {
+	return &errReader{err: err}
+
+}
+
+type errReader struct {
+	err error
+}
+
+func (r *errReader) Read(p []byte) (int, error) {
+	return 0, r.err
+}
+
+func Test_ReadError(t *testing.T) {
+	// A reader that always returns an error
+	r := ErrReader(errors.New("error"))
+
+	chunkSize := 1024
+	chunksCh := make(chan ChunkResult)
+	go splitReaderIntoChunks(context.Background(), r, chunkSize, chunksCh)
+
+	chunkResult := <-chunksCh
+	require.Error(t, chunkResult.Error)
+}
+
+func Test_ReadSuccess(t *testing.T) {
+	// Generate 10 KiB of random data
+	reader, expectedContent := randomReader(t, 1024*10)
+
+	// Split file into 1 KiB chunks
+	chunkSize := 1024
+	chunksCh := make(chan ChunkResult)
+	go splitReaderIntoChunks(context.Background(), reader, chunkSize, chunksCh)
+
+	// Create temp file in test temp dir to hold chunks
+	tempDir := t.TempDir()
+	file, err := ioutil.TempFile(tempDir, "")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	for chunkResult := range chunksCh {
+		// Write chunks at the specified offsets
+		if chunkResult.Error == nil {
+			if _, err := file.WriteAt(chunkResult.Result.Data, int64(chunkResult.Result.Offset)); err != nil {
+				t.Fatalf("failed to write at offset %v: %v", chunkResult.Result.Offset, err)
+			}
+		} else if chunkResult.Error == io.EOF {
+			// No more chunks can come if we receive an EoF
+			break
+		} else {
+			t.Fatalf("received chunk result with error: %v", chunkResult.Error)
+		}
+	}
+	// Assert content is correct (checks that offsets are correct as otherwise
+	// the content would not match)
+	gotContent, err := ioutil.ReadFile(file.Name())
+	if err != nil {
+		t.Fatalf("failed to read entire testfile: %v", err)
+	}
+
+	require.Equal(t, expectedContent, gotContent)
+}
